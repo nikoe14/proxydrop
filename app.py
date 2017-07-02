ab17b9ff58dca09b977fbc2b41918badfe6bab86
Nicolas <nico@nico-pc>
2017-07-02 03:19:11 -0300

    version 1.0 working

--- app.py
+++ app.py
@@ -4,12 +4,30 @@ import subprocess
 app = Flask(__name__)
 ip_whitelist = ['172.16.0.251', '127.0.0.1'] 
 
-def valid_ip(): 
+def white_list(): 
 	client = request.remote_addr 
 	if client in ip_whitelist: 
 		return True 
 	else: 
 		return False
+
+def validate_ip(ip):
+    a = ip.split('.')
+    if len(a) != 4:
+        return False
+    for x in a:
+        if not x.isdigit():
+            return False
+        i = int(x)
+        if i < 0 or i > 255:
+            return False
+    return True
+
+def validate_dropaverage(a):
+    if (int(a) > 0) and (int(a) <= 99):
+        return True
+    else:
+        return False
 
 @app.route('/')
 def form():
@@ -17,26 +35,25 @@ def form():
 
 @app.route('/hello/', methods=['POST'])
 def hello():
-  dropavarage=request.form['dropavarage']
+  dropaverage=request.form['dropaverage']
   target=request.form['target']
-  return render_template('form_action.html', dropavarage=dropavarage, target=target)
+  return render_template('form_action.html', dropaverage=dropaverage, target=target)
 
 @app.route('/status/', methods=['POST']) 
 def status(): 
-#    if valid_ip(): 
-        target=request.form['target']
-#    subprocess.Popen(['ls'], stdout=subprocess.PIPE).communicate()[0]
-	try: 
-	    subprocess.Popen(['iptables', '-A', 'OUTPUT', '-m', 'statistic', '--mode', 'random', '--probability', '0.5', '-j', 'DROP', '-d', target], stdout=subprocess.PIPE)
-        except subprocess.CalledProcessError as e: 
-            return "An error occurred while trying to fetch task status updates."  
-	return render_template('form_submit.html')
-
-#        return 'Success %s, Pending %s, Failed %s' % (result_success, result_pending, result_failed) 
-#    else: 
-#        return """<title>404 Not Found</title> 
-#      <h1>Not Found</h1> 
-#      <p>The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again.</p>""", 404 
+    target=request.form['target']
+    dropaverage=request.form['dropaverage']
+    if validate_dropaverage(dropaverage):
+        if validate_ip(target) and white_list(): 
+            try: 
+                subprocess.Popen(['iptables', '-A', 'OUTPUT', '-m', 'statistic', '--mode', 'random', '--probability', '0.'+dropaverage, '-j', 'DROP', '-d', target], stdout=subprocess.PIPE)
+            except subprocess.CalledProcessError as e: 
+                return "An error occurred while trying to fetch task status updates."  
+            return render_template('form_success.html', dropaverage=dropaverage, target=target)
+        else:
+            return render_template('form_error.html', error='the IP')
+    else:
+        return render_template('form_error.html', error='the drop average. Remember that it has to be a number between 1 and 99')
 
 # Run the app :)
 if __name__ == '__main__':
