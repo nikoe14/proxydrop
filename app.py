b1ac17a9a50fa06c46239e46ff2513c9beb4b526
Nicolas <nico@nico-pc>
2017-07-05 01:11:27 -0300

    working 1.0

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ import subprocess
 app = Flask(__name__)
 ip_whitelist = ['127.0.0.1']
 
-def white_list(): 
-	client = request.remote_addr 
-	if client in ip_whitelist: 
-		return True 
-	else: 
-		return False
+def white_list():
+    client = request.remote_addr
+    if client in ip_whitelist:
+        return True
+    else:
+        return False
 
 def validate_ip(ip):
     a = ip.split('.')
@@ -23,50 +23,94 @@ def validate_ip(ip):
             return False
     return True
 
+def array_size(a):
+    if len(a) > 2:
+        return True
+    else:
+        return False
+
 def validate_dropaverage(a):
     if (int(a) > 0) and (int(a) <= 99):
         return True
     else:
         return False
 
+def rules():
+    try:
+        com = subprocess.Popen(['iptables', '-L', 'OUTPUT', '--line-numbers'], stdout=subprocess.PIPE)
+        output = b','.join(com.stdout).decode('utf-8')
+        output = output.split(',')
+        if array_size(output):
+            del output[0]
+            del output[0]
+        else:
+            return "NIL"
+    except subprocess.CalledProcessError as e:
+        return "An error occurred while trying to show rules."
+    return output
+
+def del_rule(line):
+    try:
+        subprocess.Popen(['iptables', '-D', 'OUTPUT', line], stdout=subprocess.PIPE)
+    except subprocess.CalledProcessError as e:
+        return False
+    return True
+
 @app.route('/')
 def form():
-    return render_template('form_submit.html')
-
-@app.route('/newrule/', methods=['POST']) 
-def newrule(): 
-    target=request.form['target']
-    dropaverage=request.form['dropaverage']
-    if validate_dropaverage(dropaverage):
-        if validate_ip(target) and white_list(): 
-            try: 
-                subprocess.Popen(['iptables', '-A', 'OUTPUT', '-m', 'statistic', '--mode', 'random', '--probability', '0.'+dropaverage, '-j', 'DROP', '-d', target], stdout=subprocess.PIPE)
-            except subprocess.CalledProcessError as e: 
-                return "An error occurred while trying to fetch task status updates."  
-            return render_template('form_success.html', dropaverage=dropaverage, target=target)
-        else:
-            return render_template('form_error.html', error='the IP')
+    data = rules()
+    if data == "NIL":
+        return render_template('form_submit.html', msg="Nothing to Show")
     else:
-        return render_template('form_error.html', error='the drop average. Remember that it has to be a number between 1 and 99')
+        return render_template('form_submit.html', iptables=data)
 
-@app.route('/show/', methods=['POST']) 
-def show(): 
-#    if validate_ip(target) and white_list(): 
-        try: 
-            com = subprocess.Popen(['iptables', '-L', 'OUTPUT', '--line-numbers'], stdout=subprocess.PIPE)
-            output = b''.join(com.stdout).decode('utf-8')
-        except subprocess.CalledProcessError as e: 
-            return "An error occurred while trying to fetch task status updates."  
-        return render_template('form_show.html', iptables=output)
-#    else:
-#        return render_template('form_error.html', error='the IP')
 
+@app.route('/newrule/', methods=['POST'])
+def newrule():
+    target = request.form['target']
+    dropaverage = request.form['dropaverage']
+    if (target != "") and (dropaverage != ""):
+        if validate_dropaverage(dropaverage):
+            if validate_ip(target) and white_list():
+                try:
+                    subprocess.Popen(['iptables', '-A', 'OUTPUT', '-m', 'statistic', '--mode', 'random', '--probability', '0.'+dropaverage, '-j', 'DROP', '-d', target], stdout=subprocess.PIPE)
+                except subprocess.CalledProcessError as e:
+                    return "An error occurred while trying to add a new rule."
+                return render_template('form_success.html', msg="Success! droping " +dropaverage +"% of traffic to " +target)
+            else:
+                return render_template('form_error.html', error='the IP')
+        else:
+            return render_template('form_error.html', error='the drop average. Remember that it has to be a number between 1 and 99')
+    else:
+        return render_template('form_error.html', error='IP and drop avarage. Remember that fields can not be empty')
+
+@app.route('/show/', methods=['GET'])
+def show():
+    data = rules()
+    if data == "NIL":
+        return render_template('form_show.html', msg="Nothing to Show")
+    else:
+        return render_template('form_show.html', iptables=data)
+
+@app.route('/delete/', methods=['GET'])
+def delete():
+    data = rules()
+    if data == "NIL":
+        return render_template('form_delete_rule.html', msg="Nothing to Show")
+    else:
+        return render_template('form_delete_rule.html', iptables=data)
+    print request.form['rule']
+
+@app.route('/delete_rule/', methods=['POST'])
+def delete_rule():
+    del_rule(request.form['rule'])
+    return render_template('form_success.html', msg="Success!, The Rule was Delete")
 
 # Run the app :)
 if __name__ == '__main__':
-    app.run( 
+    app.run(
         host="0.0.0.0",
         port=int("80")
-  )
+        )
 
 app.config["CACHE_TYPE"] = "null"
